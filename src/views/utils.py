3a1b706cfb7e6b037eb146bb4e22325ff25190fb
giovane <giovaneliberato@gmail.com>
2013-11-05 11:47:52 -0200

    removendo arquivos realocados

--- src/views/utils.py
+++
@@ -1,6 +0,0 @@
-from pyramid.renderers import get_renderer
-
-def base_layout():
-    renderer = get_renderer("../templates/base.pt")
-    layout = renderer.implementation().macros['layout']
-    return layout
